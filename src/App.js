07eb8634608fc4288b7df1ebcfa7f6121f5c9414
Reberth da Silva <reberth.silva@kroton.com.br>
2019-02-23 23:25:11 -0300

    verifying what component will display, mocking URL and without React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: []
+    contacts: [],
+    screen: 'list'
   }
 
   componentDidMount() {
@@ -22,10 +24,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts
+        {this.state.screen === 'list' && (<ListContacts
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
-        />
+        />)}
+        {this.state.screen === 'create' && (<CreateContact />)}
       </div>
     )
   }
