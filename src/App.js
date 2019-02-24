92f30f7d330897a6906b0a93166d108b0d527f9c
Reberth da Silva <reberth.silva@kroton.com.br>
2019-02-24 12:55:40 -0300

    doing verification with react router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: 'list'
+    contacts: []
   }
 
   componentDidMount() {
@@ -24,16 +24,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (<ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={() => {
-            this.setState({
-              screen: 'create'
-            })
-          }}
-        />)}
-        {this.state.screen === 'create' && (<CreateContact />)}
+        <Route exact path="/" render={() =>
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />}
+        />
+        <Route path="/create" component={CreateContact} />
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -21,7 +22,7 @@ class ListContacts extends Component {
 
   render() {
     const { query } = this.state;
-    const { contacts, onDeleteContact, onNavigate } = this.props;
+    const { contacts, onDeleteContact } = this.props;
 
     const showingContacts = query === ''
       ? contacts :
@@ -37,7 +38,7 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a href="#create" onClick={onNavigate} className="add-contact">Add Contact</a>
+          <Link to="/create" className="add-contact">Add Contact</Link>
         </div>
 
         {showingContacts.length !== contacts.length &&
