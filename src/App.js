c1496003ef663c447e1b1a1b7ab88ae8f5bd5e7d
Reberth da Silva <reberth.silva@kroton.com.br>
2019-02-23 22:00:12 -0300

    Removing contact by API

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
     this.setState(currentState => ({
       contacts: currentState.contacts.filter(c => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
 
   render() {
